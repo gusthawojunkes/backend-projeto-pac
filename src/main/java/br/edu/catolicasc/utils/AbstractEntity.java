a4b424ae5487206c56343393b3f71beefffedd60
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2021-06-08 13:31:29 -0300

    First steps for AbstractEntity

--- src/main/java/br/edu/catolicasc/utils/AbstractEntity.java
+++ src/main/java/br/edu/catolicasc/utils/AbstractEntity.java
@@ -2,21 +2,43 @@ package br.edu.catolicasc.utils;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.Setter;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
+
+import static javax.persistence.TemporalType.TIMESTAMP;
 
 @Getter
+@Setter
 @EqualsAndHashCode
 @MappedSuperclass
-public class AbstractEntity implements Serializable {
+public abstract class AbstractEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    protected Long id;
 
+    @Temporal(TIMESTAMP)
+    @Column(name = "created_at")
+    protected Date createdAt;
+
+    @Temporal(TIMESTAMP)
+    @Column(name = "updated_at")
+    protected Date updatedAt;
+
+    @PrePersist
+    @PreUpdate
+    private void save(){
+        if (getId() == null) {
+            setCreatedAt(new Date());
+        }
+        setUpdatedAt(new Date());
+    }
+
+    public abstract Long getId();
+
+    public abstract void setId(Long id);
 
 }
