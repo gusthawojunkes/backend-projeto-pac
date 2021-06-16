3b1d60be18fc7529e9a0af5e1914ba22947436f9
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2021-06-15 22:36:31 -0300

    TODO: Create School Class

---
+++ src/main/java/br/edu/catolicasc/pac/config/school/School.java
@@ -0,0 +1,16 @@
+package br.edu.catolicasc.pac.config.school;
+
+import br.edu.catolicasc.utils.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@Entity
+public class School extends AbstractEntity {
+
+    
+
+}
