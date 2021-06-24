a0f3acd5d9dd244575885f9d895b7951e9fb85b3
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2021-06-24 13:31:28 -0300

    Preparando lista de grupos para o front-end

--- src/main/java/br/edu/catolicasc/pac/config/group/controller/UserGroupController.java
+++ src/main/java/br/edu/catolicasc/pac/config/group/controller/UserGroupController.java
@@ -5,21 +5,34 @@ import br.edu.catolicasc.pac.config.group.model.UserGroupModel;
 import br.edu.catolicasc.pac.config.user.User;
 import br.edu.catolicasc.pac.config.user.model.UserModel;
 import br.edu.catolicasc.pac.repository.config.UserGroupRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/groups")
 public class UserGroupController {
 
     private final UserGroupRepository repo;
 
+    @Autowired
     public UserGroupController(UserGroupRepository repo) {
         this.repo = repo;
+    }
+
+    @GetMapping
+    public List<UserGroupModel> findAll() {
+        List<UserGroupModel> listReturn = new ArrayList<>();
+        Iterable<UserGroup> groups = repo.findAll();
+        for(UserGroup group : groups) {
+            UserGroupModel model = UserGroupModel.getModelByUserGroup(group);
+            listReturn.add(model);
+        }
+        return listReturn;
     }
 
     @PostMapping("/create")
--- src/main/java/br/edu/catolicasc/pac/config/group/model/UserGroupModel.java
+++ src/main/java/br/edu/catolicasc/pac/config/group/model/UserGroupModel.java
@@ -1,10 +1,15 @@
 package br.edu.catolicasc.pac.config.group.model;
 
+import br.edu.catolicasc.pac.config.action.Action;
 import br.edu.catolicasc.pac.config.action.model.ActionModel;
+import br.edu.catolicasc.pac.config.group.UserGroup;
+import br.edu.catolicasc.pac.config.permission.Permission;
 import br.edu.catolicasc.pac.config.permission.model.PermissionModel;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -19,5 +24,37 @@ public class UserGroupModel {
 
     @JsonProperty("action")
     private ActionModel action;
+
+    public static UserGroupModel getModelByUserGroup(UserGroup group) {
+        UserGroupModel model = new UserGroupModel();
+        PermissionModel permModel = new PermissionModel();
+        ActionModel actModel = new ActionModel();
+
+        model.setName(group.getName());
+        model.setDescription(group.getDescription());
+
+        Permission permission = group.getPermission();
+
+        permModel.setCode(permission.getCode());
+        permModel.setDescription(permission.getDescription());
+        permModel.setViewChallenges(permission.getViewChallenges());
+        permModel.setViewRanking(permission.getViewRanking());
+        permModel.setViewReports(permission.getViewReports());
+        permModel.setViewConfig(permission.getViewConfig());
+
+        Action action = group.getAction();
+
+        actModel.setConfChallenges(action.getConfChallenges());
+        actModel.setPlayChallenges(action.getPlayChallenges());
+        actModel.setConfUsers(action.getConfUsers());
+        actModel.setViewUsers(action.getViewUsers());
+        actModel.setConfGroups(action.getConfGroups());
+        actModel.setViewGroups(action.getViewGroups());
+
+        model.setPermission(permModel);
+        model.setAction(actModel);
+
+        return model;
+    }
 
 }
