9c59aa83adb97ec299076a5b3d12312d16d828de
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2021-06-25 13:27:58 -0300

    Endpoint update for usergroup

--- src/main/java/br/edu/catolicasc/pac/config/group/controller/UserGroupController.java
+++ src/main/java/br/edu/catolicasc/pac/config/group/controller/UserGroupController.java
@@ -2,8 +2,10 @@ package br.edu.catolicasc.pac.config.group.controller;
 
 import br.edu.catolicasc.pac.config.group.UserGroup;
 import br.edu.catolicasc.pac.config.group.model.UserGroupModel;
+import br.edu.catolicasc.pac.config.user.User;
 import br.edu.catolicasc.pac.repository.config.UserGroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,6 +39,15 @@ public class UserGroupController {
     @PostMapping("/create")
     public UserGroup create(@RequestBody UserGroupModel model) throws ParseException {
         return repo.save(new UserGroup(model));
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<?> update(@RequestBody UserGroupModel group) {
+        if (group == null) return new ResponseEntity<>("Group not found", HttpStatus.NO_CONTENT);
+        return repo.findById(group.getId()).map(record -> {
+            UserGroup updated = repo.save(record);
+            return ResponseEntity.ok().body(updated);
+        }).orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/delete/{id}")
